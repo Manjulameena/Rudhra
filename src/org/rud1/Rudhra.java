94534e94da1db2c7c9ce4643cf28ec28330916b5
Manjulameena <mailtomanjulamurugan@gmail.com>
2022-11-22 13:56:02 +0530

    code added

---
+++ src/org/rud1/Rudhra.java
@@ -0,0 +1,12 @@
+package org.rud1;
+
+public class Rudhra {
+public static void main(String[] args) {
+	System.out.println("code1");
+	System.out.println("code2");
+	System.out.println("code3");
+}
+
+
+}
+
